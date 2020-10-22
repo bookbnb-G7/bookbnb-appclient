f03d0b18f3cd04124ea311e09a2af6defd0651df
Agustin Leguizamon <agustinmleguizamon@gmail.com>
2020-10-22 18:14:07 -0300

    Fixes to Get and Post Buttons

--- app/screens/ReactFetchScreen.js
+++ app/screens/ReactFetchScreen.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Text, View, StyleSheet, Button, TextInput } from "react-native";
+import { Text, View, StyleSheet, Button, TextInput, Alert } from "react-native";
 
 var NUMBER = "0123456789";
 
@@ -14,7 +14,7 @@ function isATextNumber(text) {
   return isANumber;
 }
 
-function Ajax(props) {
+function ReactFetchScreen(props) {
   //https://es.reactjs.org/docs/hooks-state.html
 
   /** Hook permite usar estado y otras caracteristicas sin escribir una clase*/
@@ -34,24 +34,35 @@ function Ajax(props) {
 
   const [error, setError] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
-  const [isCorrectId, setIsCorrectId] = useState(false);
+
   const [title, setTitle] = useState("string");
   const [description, setDescription] = useState("string");
-  const [id, setId] = useState(1);
+  const [_author, setAuthor] = useState("string");
 
-  const [text, setText] = useState("");
+  const [_id, setId] = useState(0);
+  const [_input_id, setInputId] = useState(1);
+
+  const [_id_text, setText] = useState("");
+  const [_title_text, setTitleText] = useState("");
+  const [_description_text, setDescriptionText] = useState("");
+
+  const [_is_post, setIsPost] = useState(false);
+  const [_is_get, setIsGet] = useState(false);
 
   /**Como estoy usando Hooks y no clase esto lo miro como metodos de la clase del componente
    * por eso los marco como privados
    */
   function _handleGetButtonPress() {
-    console.log(isATextNumber({ text }));
-    if (isATextNumber({ text })) {
-      setId(parseInt(text));
+    console.log("Get:" + isATextNumber({ _id_text }));
+    if (isATextNumber({ _id_text })) {
+      setInputId(parseInt(_id_text));
+      setIsGet(true);
     }
   }
 
-  const ph = () => console.log("placeholder handler");
+  function _handlePostButtonPress() {
+    setIsPost(true);
+  }
 
   // Note: the empty deps array [] means
   // this useEffect will run once
@@ -66,49 +77,95 @@ function Ajax(props) {
   /** Sometimes, we want to run some additional code after React has updated the DOM.
    * Network requests, manual DOM mutations, and logging are common examples of effects
    * that don’t require a cleanup.*/
-  console.log("id:" + id);
-  let url = "https://bookbnb-appserver.herokuapp.com/notes/" + id;
+
+  let url = "https://bookbnb-appserver.herokuapp.com/notes/" + _input_id;
   console.log("url:" + url);
   useEffect(() => {
-    fetch(url)
-      .then((res) => res.json())
-      //.then((json) => console.log(json))
-      .then(
-        (result) => {
-          setIsLoaded(true);
-          setTitle(result.title);
-          setDescription(result.description);
-          setId(result.id);
-        },
-        // Nota: es importante manejar errores aquí y no en
-        // un bloque catch() para que no interceptemos errores
-        // de errores reales en los componentes.
-        (error) => {
-          setIsLoaded(true);
-          setError(error);
-        }
-      );
-    /** empty dependency array means this 
-    effect will only run once (like 
-    componentDidMount in classes)*/
-  }, []);
-
+    if ((_is_get && _input_id > 0) || !isLoaded) {
+      fetch(url)
+        .then((res) => res.json())
+        .then(
+          (result) => {
+            setIsLoaded(true);
+            setTitle(result.title);
+            setDescription(result.description);
+            setId(result.id);
+          },
+          (error) => {
+            setIsLoaded(true);
+            setError(error);
+          }
+        );
+    } else if (_is_get) {
+      Alert.alert("Debe completar el campo id");
+    }
+    return () => {
+      setIsGet(false);
+    };
+  }, [_is_get]);
+  /** 
+  url = "https://bookbnb-appserver.herokuapp.com/posts/" + _id;
+  console.log("url:" + url);
   useEffect(() => {
-    //POST request using fetch inside useEffect
-    /**El formato lo podes sacar viendo el try it out
-     * the FastApi que te muestra el Curl para el POST*/
+    if (POST){}
+    fetch(url)
+      .then((response) => response.json())
+      //.then((json) => console.log(json))
+      .then((result) => {
+        setIsLoaded(true);
+        setIsPostsLoaded(true);
+        setTitle(result.title);
+        setDescription(result.body);
+        setAuthor(result.author);
+        setId(result.id);
+      });
+  }, []);
+*/
+  //POST request using fetch inside useEffect
+  /**El formato lo podes sacar viendo el try it out
+   * the FastApi que te muestra el Curl para el POST*/
+
+  console.log("posting:" + _is_post);
+  useEffect(() => {
+    if (_is_post && _title_text !== "" && _description_text !== "") {
+      console.log("UseEffect");
+      const requestOptions = {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          title: _title_text,
+          description: _description_text,
+        }),
+      };
+      fetch("https://bookbnb-appserver.herokuapp.com/notes/", requestOptions)
+        .then((response) => response.json())
+        .then((json) => console.log(json));
+    } else if (_is_post) {
+      Alert.alert("Debe completar los campos title y description");
+    }
+    return () => {
+      console.log("CleanUp");
+      setIsPost(false);
+    };
+  }, [_is_post]);
+
+  /** 
+  useEffect(() => {
     const requestOptions = {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
-        title: "ReactNative",
-        description: "ReactNative",
+        title: "ReactNativePost",
+        body: "posts",
+        author:"Agus",
+
       }),
     };
-    fetch("https://bookbnb-appserver.herokuapp.com/notes/", requestOptions)
+    fetch("https://bookbnb-appserver.herokuapp.com/posts/", requestOptions)
       .then((response) => response.json())
       .then((json) => console.log(json));
   }, []);
+  */
 
   if (error) {
     return <Text>Error: {error.message}</Text>;
@@ -124,21 +181,30 @@ function Ajax(props) {
         <Text style={styles.text}>
           Para Get indicar un id y presionar GET, para un POST apretar SEND
         </Text>
-        <TextInput
-          style={styles.textContainer}
-          placeholder="Id"
-          onChangeText={(text) => setText(text)}
-          defaultValue={text}
-        ></TextInput>
-        <Text style={styles.textContainer}>DEBUG TEXT: {text}</Text>
+        <View style={styles.textContainer}>
+          <TextInput
+            placeholder="id: "
+            onChangeText={(_id_text) => setText(_id_text)}
+          ></TextInput>
+          <TextInput
+            placeholder="title: "
+            onChangeText={(_title_text) => setTitleText(_title_text)}
+          ></TextInput>
+          <TextInput
+            placeholder="description: "
+            onChangeText={(_description_text) =>
+              setDescriptionText(_description_text)
+            }
+          ></TextInput>
+        </View>
         <View style={styles.buttonContainer}>
           <Button title="Get" onPress={_handleGetButtonPress}></Button>
-          <Button title="Send" onPress={ph}></Button>
-          <Button title="Reset" onPress={ph}></Button>
+          <Button title="Post" onPress={_handlePostButtonPress}></Button>
         </View>
+        <Text> JSON Response </Text>
         <Text> title: {title} </Text>
         <Text> description: {description}</Text>
-        <Text> id: {id}</Text>
+        <Text> id: {_id}</Text>
       </View>
     );
   }
@@ -148,6 +214,7 @@ const styles = StyleSheet.create({
   mainContainer: {
     flex: 1,
     justifyContent: "center",
+    backgroundColor: "#3498db",
   },
   text: {
     textAlign: "center",
@@ -155,15 +222,17 @@ const styles = StyleSheet.create({
   textContainer: {
     backgroundColor: "lightblue",
     marginBottom: 10,
-    paddingHorizontal: 20,
+    paddingHorizontal: 10,
   },
   buttonContainer: {
     padding: 10,
   },
 });
 
-export default Ajax;
+export default ReactFetchScreen;
 
 /**TODO @AgustinLeguizamon Como Hacer para correr
  * el UseEffect bajo demanda en vez de que
- * suceda una y cada vez que se inicia la app*/
+ * suceda una y cada vez que se inicia la app
+ * Hay que poner las condiciones en el array [] al final del useEffect, si cambia el valor que coloco ahi
+ * vuelve a disparar el effect en el siguiente renderizado e.g. [_id], si _id cambia se ejecuta el useEffect*/
