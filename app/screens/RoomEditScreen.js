1273fab809c1766f3fb7aa80fc8387f808ca17b2
Agustin Leguizamon <agustinmleguizamon@gmail.com>
2020-11-19 12:07:01 -0300

    Added RoomScreen PATCH

--- app/screens/RoomEditScreen.js
+++ app/screens/RoomEditScreen.js
@@ -8,14 +8,44 @@ import Separator from "../components/Separator";
 import colors from "../config/colors";
 import fonts from "../config/fonts";
 import styling from "../config/styling";
+import httpPostRequest from "../helpers/httpPostRequest";
 
 function RoomEditScreen({ route, navigation }) {
   const { room } = route.params;
   const [_is_editing, setIsEditing] = useState(false);
   const [_is_owner, setIsOwner] = useState(true);
 
+  const privates = ["id", "at", "owner"];
+  const _isPrivate = (s_key) => {
+    let i = 0;
+    let is_private = false;
+    do {
+      is_private = Boolean(s_key.search(privates[i]) >= 0);
+      i++;
+    } while (i < privates.length && !is_private);
+
+    return is_private;
+  };
+
   const _handleEditRoomButtonPress = () => {
-    setIsEditing(!_is_editing);
+    setIsEditing(true);
+  };
+
+  const _handleApiResponse = (data) => {
+    alert(JSON.stringify(data));
+  };
+
+  const _handleFinishEditingButtonPress = () => {
+    setIsEditing(false);
+    httpPostRequest(
+      "PATCH",
+      "http://bookbnb-appserver.herokuapp.com/rooms/" + room.id,
+      {
+        type: room.type,
+        price_per_day: room.price_per_day,
+      },
+      _handleApiResponse
+    );
   };
 
   return (
@@ -23,17 +53,18 @@ function RoomEditScreen({ route, navigation }) {
       <BnbBodyView>
         {Object.entries(room).map(([key, value]) => (
           <View key={key}>
-            <View style={styles.rowRoomElement}>
-              <Text style={styles.keyText}>{key}</Text>
-              <TextInput
-                style={styles.valueText}
-                defaultValue={value.toString()}
-                editable={_is_editing}
-                onChangeText={(text) => (room[key] = text)}
-                multiline
-              ></TextInput>
-            </View>
-            <Separator />
+            {!_isPrivate(key) && (
+              <View style={styles.rowRoomElement}>
+                <Text style={styles.keyText}>{key}</Text>
+                <TextInput
+                  style={styles.valueText}
+                  defaultValue={value.toString()}
+                  editable={_is_editing}
+                  onChangeText={(text) => (room[key] = text)}
+                  multiline
+                ></TextInput>
+              </View>
+            )}
           </View>
         ))}
         {_is_owner && (
@@ -41,7 +72,11 @@ function RoomEditScreen({ route, navigation }) {
             <BnbButton
               style={styles.button}
               title={_is_editing ? "Aceptar" : "Editar habitacion"}
-              onPress={_handleEditRoomButtonPress}
+              onPress={
+                _is_editing
+                  ? _handleFinishEditingButtonPress
+                  : _handleEditRoomButtonPress
+              }
             ></BnbButton>
           </View>
         )}
@@ -56,6 +91,8 @@ const styles = StyleSheet.create({
   },
   rowRoomElement: {
     flexDirection: "row",
+    marginVertical: styling.separator,
+    borderBottomWidth: 1,
   },
   keyText: {
     fontSize: fonts.semi,
