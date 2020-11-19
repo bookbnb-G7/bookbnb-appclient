3c02bec49ef6c7c77823d7ac801444983147adf0
Agustin Leguizamon <agustinmleguizamon@gmail.com>
2020-11-19 12:24:40 -0300

    Removed innecesary Object entries showing in RoomEditScreen.js

--- app/screens/RoomEditScreen.js
+++ app/screens/RoomEditScreen.js
@@ -4,6 +4,7 @@ import { TextInput } from "react-native-gesture-handler";
 import BnbBodyView from "../components/BnbBodyView";
 import BnbButton from "../components/BnbButton";
 import BnbMainView from "../components/BnbMainView";
+import BnbTitleText from "../components/BnbTitleText";
 import Separator from "../components/Separator";
 import colors from "../config/colors";
 import fonts from "../config/fonts";
@@ -15,20 +16,8 @@ function RoomEditScreen({ route, navigation }) {
   const [_is_editing, setIsEditing] = useState(false);
   const [_is_owner, setIsOwner] = useState(true);
 
-  const privates = ["id", "at", "owner"];
-  const _isPrivate = (s_key) => {
-    let i = 0;
-    let is_private = false;
-    do {
-      is_private = Boolean(s_key.search(privates[i]) >= 0);
-      i++;
-    } while (i < privates.length && !is_private);
-
-    return is_private;
-  };
-
-  const _handleEditRoomButtonPress = () => {
-    setIsEditing(true);
+  const _handleToggleEditRoomButtonPress = () => {
+    setIsEditing(!_is_editing);
   };
 
   const _handleApiResponse = (data) => {
@@ -51,35 +40,52 @@ function RoomEditScreen({ route, navigation }) {
   return (
     <BnbMainView style={styles.white}>
       <BnbBodyView>
-        {Object.entries(room).map(([key, value]) => (
-          <View key={key}>
-            {!_isPrivate(key) && (
-              <View style={styles.rowRoomElement}>
-                <Text style={styles.keyText}>{key}</Text>
-                <TextInput
-                  style={styles.valueText}
-                  defaultValue={value.toString()}
-                  editable={_is_editing}
-                  onChangeText={(text) => (room[key] = text)}
-                  multiline
-                ></TextInput>
-              </View>
-            )}
-          </View>
-        ))}
-        {_is_owner && (
-          <View style={styles.buttonsContainer}>
+        <BnbTitleText style={styles.subTitle}>
+          Detalles de la habitacion
+        </BnbTitleText>
+        <Separator />
+        <View style={styles.rowRoomElement}>
+          <Text style={styles.keyText}>Nombre(tipo): </Text>
+          <TextInput
+            style={styles.valueText}
+            defaultValue={room.type.toString()}
+            editable={_is_editing}
+            onChangeText={(text) => (room["type"] = text)}
+            multiline
+          ></TextInput>
+        </View>
+        <Separator></Separator>
+        <View style={styles.rowRoomElement}>
+          <Text style={styles.keyText}>Precio por dia: </Text>
+          <TextInput
+            style={styles.valueText}
+            defaultValue={room.price_per_day.toString()}
+            editable={_is_editing}
+            onChangeText={(text) => (room["price_per_day"] = text)}
+            multiline
+          ></TextInput>
+        </View>
+
+        <View style={styles.buttonsContainer}>
+          {_is_owner && (
             <BnbButton
               style={styles.button}
               title={_is_editing ? "Aceptar" : "Editar habitacion"}
               onPress={
                 _is_editing
                   ? _handleFinishEditingButtonPress
-                  : _handleEditRoomButtonPress
+                  : _handleToggleEditRoomButtonPress
               }
             ></BnbButton>
-          </View>
-        )}
+          )}
+          {_is_editing && (
+            <BnbButton
+              style={styles.button}
+              title={"Cancelar"}
+              onPress={_handleToggleEditRoomButtonPress}
+            ></BnbButton>
+          )}
+        </View>
       </BnbBodyView>
     </BnbMainView>
   );
@@ -89,10 +95,12 @@ const styles = StyleSheet.create({
   white: {
     backgroundColor: "white",
   },
+  subTitle: {
+    color: "black",
+    fontSize: fonts.bigBig,
+  },
   rowRoomElement: {
     flexDirection: "row",
-    marginVertical: styling.separator,
-    borderBottomWidth: 1,
   },
   keyText: {
     fontSize: fonts.semi,
@@ -104,8 +112,10 @@ const styles = StyleSheet.create({
     backgroundColor: colors.graySoft,
   },
   buttonsContainer: {
-    marginVertical: styling.separator,
+    marginVertical: 20,
     alignItems: "center",
+    flexDirection: "row",
+    justifyContent: "space-between",
   },
 });
 
