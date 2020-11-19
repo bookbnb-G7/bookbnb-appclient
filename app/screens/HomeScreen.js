142232174fa0af5126016047ab3c85860d7d4654
Agustin Leguizamon <agustinmleguizamon@gmail.com>
2020-11-19 14:27:48 -0300

    Added background image to home

--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -1,12 +1,19 @@
 import React from "react";
-import { View, StyleSheet, Text, Image } from "react-native";
+import {
+  View,
+  StyleSheet,
+  Text,
+  Image,
+  ImageBackground,
+  Dimensions,
+} from "react-native";
 import BnbBodyView from "../components/BnbBodyView";
 import BnbButton from "../components/BnbButton";
-import BnbFooterView from "../components/BnbFooterView";
 import BnbMainView from "../components/BnbMainView";
-import Separator from "../components/Separator";
+import colors from "../config/colors";
 
-bnb_book_logo = require("../assets/airbnb.png");
+const bnb_book_logo = require("../assets/Bookbnb_logo.png");
+const background = require("../assets/background_2.png");
 
 function HomeScreen({ navigation }) {
   function _handleSearchRoomsButton() {
@@ -18,32 +25,50 @@ function HomeScreen({ navigation }) {
   };
 
   return (
-    <BnbMainView style={{ backgroundColor: "white" }}>
-      <BnbBodyView>
-        <Image style={styles.logo} source={bnb_book_logo}></Image>
-        <View style={styles.optionsContainer}>
-          <BnbButton
-            onPress={_handleSearchRoomsButton}
-            title={"Buscar Habitaciones"}
-          />
-          <BnbButton onPress={_handleSearchUsersButton} title={"Login"} />
-        </View>
+    <BnbMainView style={styles.white}>
+      <Image style={styles.logo} source={bnb_book_logo}></Image>
+      <BnbBodyView style={styles.bodyContainer}>
+        <ImageBackground source={background} style={styles.background}>
+          <View style={styles.optionsContainer}>
+            <BnbButton
+              onPress={_handleSearchRoomsButton}
+              title={"Buscar Habitaciones"}
+            />
+            <BnbButton onPress={_handleSearchUsersButton} title={"Login"} />
+          </View>
+        </ImageBackground>
       </BnbBodyView>
     </BnbMainView>
   );
 }
 
+const dimensions = Dimensions.get("window");
+
 const styles = StyleSheet.create({
+  white: {
+    backgroundColor: colors.graySoft,
+  },
   optionsContainer: {
-    flex: 2,
+    flex: 1,
     marginVertical: 10,
     alignItems: "center",
     justifyContent: "space-evenly",
+  },
+  bodyContainer: {
+    backgroundColor: "rgba(0, 0, 0, 0)",
+    borderWidth: 1,
+    paddingHorizontal: 0,
+    paddingVertical: 0,
   },
   logo: {
     width: 100,
     height: 100,
     alignSelf: "center",
+  },
+  background: {
+    flex: 1,
+    //width: "100%",
+    //resizeMode: "repeat",
   },
 });
 
