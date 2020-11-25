6e050d2fab77e75f064ff24d0ea892a9b8b14fad
Agustin Leguizamon <agustinmleguizamon@gmail.com>
2020-11-25 18:23:00 -0300

    Removed alerts and replaced it with a message box

--- app/helpers/httpPostRequest.js
+++ app/helpers/httpPostRequest.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const httpPostRequest = (method, url, body, onResponse) => {
+const httpPostRequest = (method, url, body, onResponse, onError) => {
   const requestOptions = {
     method: method,
     headers: { "Content-Type": "application/json" },
@@ -8,7 +8,14 @@ const httpPostRequest = (method, url, body, onResponse) => {
   };
   fetch(url, requestOptions)
     .then((response) => response.json())
-    .then((data) => onResponse(data));
+    .then(
+      (data) => {
+        onResponse(data);
+      },
+      (error) => {
+        onError(error);
+      }
+    );
 };
 
 export default httpPostRequest;
--- app/screens/RoomCreateScreen.js
+++ app/screens/RoomCreateScreen.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Alert, StyleSheet, Text, View } from "react-native";
 import { TextInput } from "react-native-gesture-handler";
 import BnbBodyView from "../components/BnbBodyView";
 import BnbButton from "../components/BnbButton";
@@ -14,8 +14,10 @@ import isANumber from "../helpers/isANumber";
 
 function RoomCreateScreen({ route, navigation }) {
   const { user_id, user } = route.params;
-  const [_type, setRoomType] = useState("string");
+  const [_type, setRoomType] = useState("");
   const [_price_per_day, setRoomPPD] = useState(0);
+
+  const [_is_awaiting, setIsAwaiting] = useState(false);
 
   const rooms_url = "http://bookbnb-appserver.herokuapp.com/rooms/";
 
@@ -28,7 +30,31 @@ function RoomCreateScreen({ route, navigation }) {
   };
 
   const _handleApiResponse = (data) => {
-    alert(JSON.stringify(data));
+    Alert.alert(
+      "Creación de habitación",
+      "Habitación creada con exito",
+      [
+        {
+          text: "Entendido",
+        },
+      ],
+      { cancelable: false }
+    );
+    navigation.navigate("Profile");
+  };
+
+  const _handleApiError = (error) => {
+    Alert.alert(
+      "Error",
+      "Ocurrio un error",
+      [
+        {
+          text: error.message,
+        },
+      ],
+      { cancelable: false }
+    );
+    navigation.navigate("Profile");
   };
 
   const _handleNextButtonPress = () => {
@@ -42,8 +68,10 @@ function RoomCreateScreen({ route, navigation }) {
           owner_id: user_id,
           price_per_day: _price_per_day,
         },
-        _handleApiResponse
+        _handleApiResponse,
+        _handleApiError
       );
+      setIsAwaiting(true);
     } else {
       alert("El precio por dia debe ser un valor numerico");
     }
@@ -53,38 +81,49 @@ function RoomCreateScreen({ route, navigation }) {
     navigation.goBack();
   };
 
-  return (
-    <BnbMainView>
-      <BnbBodyView>
-        <BnbTitleText style={styles.titleText}>
-          Crear una habitación
-        </BnbTitleText>
-        <Separator />
-        <View style={styles.roomForm}>
-          <BnbTextInput
-            id="type"
-            left="Breve descripcion de la habitacion"
-            value={_type}
-            editable={true}
-            onChange={_handleTextChange}
-          />
-          <BnbTextInput
-            id="price_per_day"
-            left="Precio por dia"
-            value={_price_per_day.toString()}
-            editable={true}
-            onChange={_handleTextChange}
-          />
-          <Text>type: {_type}</Text>
-          <Text>ppd: {_price_per_day}</Text>
-        </View>
-        <BnbContainer>
-          <BnbButton title="Continuar" onPress={_handleNextButtonPress} />
-          <BnbButton title="Cancelar" onPress={_handleGoBackButtonPress} />
-        </BnbContainer>
-      </BnbBodyView>
-    </BnbMainView>
-  );
+  if (_is_awaiting) {
+    return (
+      <View>
+        <Text style={{ alignSelf: "center", justifyContent: "center" }}>
+          Creando habitacion...
+        </Text>
+      </View>
+    );
+  } else {
+    return (
+      <BnbMainView>
+        <BnbBodyView>
+          <BnbTitleText style={styles.titleText}>
+            Crear una habitación
+          </BnbTitleText>
+          <Separator />
+          <View style={styles.roomForm}>
+            <BnbTextInput
+              id="type"
+              left="Breve descripcion de la habitacion"
+              value={_type}
+              editable={true}
+              onChange={_handleTextChange}
+            />
+            <BnbTextInput
+              id="price_per_day"
+              left="Precio por dia"
+              value={_price_per_day.toString()}
+              editable={true}
+              onChange={_handleTextChange}
+            />
+            <Text style={styles.titleText}>Vista previa</Text>
+            <Text>Descripcion: {_type}</Text>
+            <Text>Precio por dia: {_price_per_day}</Text>
+          </View>
+          <BnbContainer>
+            <BnbButton title="Continuar" onPress={_handleNextButtonPress} />
+            <BnbButton title="Cancelar" onPress={_handleGoBackButtonPress} />
+          </BnbContainer>
+        </BnbBodyView>
+      </BnbMainView>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
