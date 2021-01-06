54cbe6a55e65c45e23d0c5e85ce39f636d8832e8
Agustin Leguizamon <agustinmleguizamon@gmail.com>
2021-01-05 21:11:43 -0300

    minor changes

--- app/helpers/useRequestMediaLibraryPermissionsAsync.js
+++ app/helpers/useRequestMediaLibraryPermissionsAsync.js
@@ -5,7 +5,6 @@ import * as ImagePicker from "expo-image-picker";
 /**Tener en cuenta que solicitar permisos solo sucede una vez */
 async function useRequestMediaLibraryPermissionsAsync() {
   let is_granted = true;
-  console.log(Platform.OS);
   if (Platform.OS !== "web") {
     const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
     if (status !== "granted") {
